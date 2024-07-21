0b417f5a05d9119a9dfe005c35cb5b7442f23313
muhammdqodir0411 <qodiriy20021104@gmail.com>
2024-07-21 12:43:16 +0500

    lesson-react

---
+++ src/components/card/NewsCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NewsCard = () => {
+  return (
+    <div>
+        <img src="" alt="" />
+        <p>Category</p>
+        <h3>News title</h3>
+        <p>Description</p>
+        <time>Date by Creator</time>
+        <p>Isoffical</p>
+    </div>
+  )
+}
+
+export default NewsCard
---
+++ src/components/news/News.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import {news} from '../../data/news'
+import "bootstrap/dist/css/bootstrap.min.css"
+export class News extends Component {
+  render() {
+    return (
+      <section>
+        <div className="container">
+            <h2 className='text-center m-3'>News Page</h2>
+            <div className="row">
+            {news.map((el, i) => (
+              <div key={i} className="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src={el.image} alt="" />
+                <h3>{el.title}</h3>
+                <p>{el.description}</p>
+                <time>{el.date}</time>
+              </div>
+            ))}
+            </div>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default News
